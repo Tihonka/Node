da0f05dc55cc5e8220e7c779ade02cb09193df11
Aleksandra Kovalenko <al.kovalen@mail.ru>
2021-12-21 05:14:56 +0300

    homework-1

---
+++ lesson-1.js
@@ -0,0 +1,37 @@
+const colors = require('colors');
+
+const firstNumber = process.argv[2];
+const secondNumber = process.argv[3];
+
+let counter = 0;
+let textColor = 'red';
+
+if(isNaN(firstNumber) || isNaN(secondNumber) || firstNumber <= 0 || secondNumber <= 0){
+    console.log(colors.red('Введенные данные не являются положительными числами!'));
+    console.log(firstNumber, secondNumber);
+} else {
+    nextPrime:
+for (let i = +firstNumber; i <= secondNumber; i++) { 
+
+  for (let j = 2; j < i; j++) { 
+    if (i % j == 0) continue nextPrime; 
+  }
+
+  if (textColor === 'red'){
+      console.log(colors.green(i));
+      textColor = 'green';
+  } else if(textColor === 'green'){
+    console.log(colors.yellow(i));
+    textColor = 'yellow';
+  } else{
+    console.log(colors.red(i));
+    textColor = 'red';
+  }
+
+  counter++;
+}
+
+if(counter == 0){
+    console.log(colors.red('В заданном диапазоне нет простых чисел!'));
+}
+};
